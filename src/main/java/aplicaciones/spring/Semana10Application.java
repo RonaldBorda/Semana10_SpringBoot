886cc248693249cfd60501a99b67444b26e5353e
Ronald <ronald.bordab@gmail.com>
2021-10-30 16:33:48 -0500

    ultimo cambio

--- src/main/java/aplicaciones/spring/Semana10Application.java
+++ src/main/java/aplicaciones/spring/Semana10Application.java
@@ -8,12 +8,12 @@ public class Semana10Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Semana10Application.class, args);
-<<<<<<< HEAD
+
 		/* omentario de prueba */
-=======
+
 		
 		
 		/*Comentario agregado en la segunda rama sprint */
->>>>>>> sprint-1
+
 	}
 }
